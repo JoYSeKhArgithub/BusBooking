30373ee0e94c804daf248275ff3ba1da1f996264
JOYSEKHAR <bjoysekhar@gamil.com>
2025-08-13 20:01:03 +0530

    first

--- src/screens/HomeScreen.tsx
+++ src/screens/HomeScreen.tsx
@@ -1,14 +1,24 @@
-import { StyleSheet, Text, View } from 'react-native'
+import { SafeAreaView, Text, View } from 'react-native'
 import React from 'react'
+import { UserCircleIcon } from 'react-native-heroicons/solid'
+import { logout } from '../services/requests/auth'
+import Bookings from '../components/home/Bookings'
 
 const HomeScreen = () => {
   return (
-    <View>
-      <Text>HomeScreen</Text>
+    <View className='flex-1 bg-white pt-10'>
+      <SafeAreaView/>
+
+        <View className='flex-row justify-between items-center px-4 py-2'>
+          <Text className='font-okra font-semibold text-3xl'>
+            Bus Tickets
+          </Text>
+
+          <UserCircleIcon color='red' size={38} onPress={logout} />
+        </View>
+        <Bookings/>
     </View>
   )
 }
 
-export default HomeScreen
-
-const styles = StyleSheet.create({})
+export default HomeScreen;
